8d91be531afb0c4936ca220813f5a5fdab2a2569
OybekDeveloper <Programmeroybek211@gmail.com>
2025-08-06 17:00:58 +0500

    dsafs

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -48,48 +48,56 @@ export default function Cart() {
         <div className="rounded-xl bg-[#F9F9F9] p-4 md:p-6">
           <h2 className="text-lg font-semibold mb-4">{t('cart.yourOrder')}</h2>
           <div className="space-y-4">
-            {items?.map((item, index) => (
-              <div
-                key={index}
-                className="bg-white rounded-xl p-4 shadow-sm"
-              >
-                <div className="flex flex-col items-center text-center">
-                  <div className="relative w-32 h-32 shrink-0 rounded-md overflow-hidden">
-                    <CustomImage
-                      src={item?.image[0] ? `${imageUrl}${item?.image[0]}` : '/placeholder.svg'}
-                      alt="Product"
-                      width={100}
-                      height={100}
-                      className="object-contain max-w-[100px] max-h-[100px]"
-                    />
+            {items?.length > 0 ? (
+              <>
+                {items?.map((item, index) => (
+                  <div
+                    key={index}
+                    className="bg-white rounded-xl p-4 shadow-sm"
+                  >
+                    <div className="flex flex-col items-center text-center">
+                      <div className="relative w-32 h-32 shrink-0 rounded-md overflow-hidden">
+                        <CustomImage
+                          src={item?.image[0] ? `${imageUrl}${item?.image[0]}` : '/placeholder.svg'}
+                          alt="Product"
+                          width={100}
+                          height={100}
+                          className="object-contain max-w-[100px] max-h-[100px]"
+                        />
+                      </div>
+                      <h3 className="text-sm font-semibold mb-1">{getTranslatedValue(item?.name, i18n.language)}</h3>
+                      <p className="text-red-500 font-bold text-sm mb-3">{formatNumber(item?.price)} {t('common.currency')}</p>
+                      <div className="w-full flex items-center justify-center gap-4 bg-black text-white px-4 py-2 rounded-xl">
+                        <button
+                          variant="ghost"
+                          size="sm"
+                          onClick={() => decrement(item?.id)}
+                          className="w-full text-xl px-2 text-white hover:bg-transparent hover:text-white"
+                        >
+                          -
+                        </button>
+                        <span className="min-w-4 text-center text-lg font-semibold">{item?.count}</span>
+                        <button
+                          variant="ghost"
+                          size="sm"
+                          onClick={() => increment(item?.id)}
+                          className="w-full text-xl px-2 text-white hover:bg-transparent hover:text-white"
+                        >
+                          +
+                        </button>
+                      </div>
+                      <button onClick={() => removeItem(item?.id)} className="pt-2 cursor-pointer text-xs text-gray-500 hover:underline">
+                        {t('cart.buttons.remove')}
+                      </button>
+                    </div>
                   </div>
-                  <h3 className="text-sm font-semibold mb-1">{getTranslatedValue(item?.name, i18n.language)}</h3>
-                  <p className="text-red-500 font-bold text-sm mb-3">{formatNumber(item?.price)} {t('common.currency')}</p>
-                  <div className="w-full flex items-center justify-center gap-4 bg-black text-white px-4 py-2 rounded-xl">
-                    <button
-                      variant="ghost"
-                      size="sm"
-                      onClick={() => decrement(item?.id)}
-                      className="w-full text-xl px-2 text-white hover:bg-transparent hover:text-white"
-                    >
-                      -
-                    </button>
-                    <span className="min-w-4 text-center text-lg font-semibold">{item?.count}</span>
-                    <button
-                      variant="ghost"
-                      size="sm"
-                      onClick={() => increment(item?.id)}
-                      className="w-full text-xl px-2 text-white hover:bg-transparent hover:text-white"
-                    >
-                      +
-                    </button>
-                  </div>
-                  <button onClick={() => removeItem(item?.id)} className="pt-2 cursor-pointer text-xs text-gray-500 hover:underline">
-                    {t('cart.buttons.remove')}
-                  </button>
-                </div>
+                ))}
+              </>
+            ) : (
+              <div className="text-center text-sm text-gray-500">
+                {t('confirmOrder.noItems') || 'Корзина пуста'}
               </div>
-            ))}
+            )}
           </div>
         </div>
 
